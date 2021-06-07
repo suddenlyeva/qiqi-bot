a892a1d956cc20577ccb995317a1a572296ae2bc
Eva Wong <suddenlyeva@gmail.com>
2021-06-06 19:42:27 -0700

    send reply upon accepting/denying entry

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
+
+  if (msg.author.bot) {
+    return;
+  }
+
   if (msg.channel.type == 'dm') {
 
     const accept_button = new disbut.MessageButton()
@@ -50,10 +55,26 @@ client.on('message', msg => {
 
 client.on('clickButton', async (button) => {
   if (button.id === 'accept_button') {
-    const new_embed = button.message.embeds[0]
+
+    const old_embed = button.message.embeds[0]
+
+    const requester_tag = old_embed.fields[0].value
+    const requester_id = requester_tag.slice(2,-1)
+
+    const reply_embed = new Discord.MessageEmbed()
+      .setColor(0x59e665)
+      .setTitle('You have been granted access to the server!')
+      .addField(config.accept_title, config.accept_value)
+      .setThumbnail(config.accept_img)
+      .setTimestamp();
+  
+    await client.users.cache.get(requester_id).send(reply_embed);
+
+    const new_embed = old_embed
       .setColor(0x59e665)
       .setFooter('Accepted by ' + button.clicker.user.tag)
       .setTimestamp();
+
     await button.message.edit(new_embed)
     await button.defer(true)
   }
@@ -61,10 +82,26 @@ client.on('clickButton', async (button) => {
 
 client.on('clickButton', async (button) => {
   if (button.id === 'deny_button') {
-    const new_embed = button.message.embeds[0]
+
+    const old_embed = button.message.embeds[0]
+
+    const requester_tag = old_embed.fields[0].value
+    const requester_id = requester_tag.slice(2,-1)
+
+    const reply_embed = new Discord.MessageEmbed()
+      .setColor(0xee484d)
+      .setTitle('You have been denied access to the server.')
+      .addField(config.deny_title, config.deny_value)
+      .setThumbnail(config.deny_img)
+      .setTimestamp();
+
+    await client.users.cache.get(requester_id).send(reply_embed);
+
+    const new_embed = old_embed
       .setColor(0xee484d)
       .setFooter('Denied by ' + button.clicker.user.tag)
       .setTimestamp();
+
     await button.message.edit(new_embed)
     await button.defer(true)
   }
