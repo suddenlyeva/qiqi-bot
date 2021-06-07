6a4a79301990b68666feb83f63c8fb1a077a9eef
Eva Wong <suddenlyeva@gmail.com>
2021-06-06 22:24:28 -0700

    multi line verification requests and adding role upon acceptance

--- index.js
+++ index.js
@@ -4,52 +4,81 @@ const disbut = require('discord-buttons');
 const config = require('./config.js');
 disbut(client);
 let mod_channel;
+let guild;
+let member_role;
 
-client.on('ready', () => {
-  mod_channel = client.channels.cache.get(config.mod_channel_id)
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./localStorage');
+}
+
+client.on('ready', async () => {
+  mod_channel = await client.channels.cache.get(config.mod_channel_id)
+  guild = await client.guilds.fetch(config.guild_id)
+  member_role = await guild.roles.fetch(config.member_role_id)
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '~ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.on('message', msg => {
+client.on('message', async msg => {
 
   if (msg.author.bot) {
     return;
   }
 
+  if (msg.content === '~ping') {
+    msg.reply('Pong!');
+  }
+
   if (msg.channel.type == 'dm') {
 
-    const accept_button = new disbut.MessageButton()
-      .setLabel('Accept')
-      .setStyle('green')
-      .setID('accept_button')
+    if (localStorage.getItem(msg.author.id + ':status') == null) {
 
-    const deny_button = new disbut.MessageButton()
-      .setLabel('Deny')
-      .setStyle('red')
-      .setID('deny_button')
+      const accept_button = new disbut.MessageButton()
+        .setLabel('Accept')
+        .setStyle('green')
+        .setID('accept_button')
+  
+      const deny_button = new disbut.MessageButton()
+        .setLabel('Deny')
+        .setStyle('red')
+        .setID('deny_button')
+  
+      const row = new disbut.MessageActionRow()
+        .addComponent(accept_button)
+        .addComponent(deny_button);
+  
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(msg.author.tag + ' requested verification:', msg.author.avatarURL())
+        .setColor(0xf2da80)
+        .setDescription(msg.content)
+        .addField('User Info:','<@' + msg.author.id + '>')
+        .setFooter('Pending')
+        .setTimestamp();
+  
+      const request = await mod_channel.send('', {
+        component: row,
+        embed
+      })
 
-    const row = new disbut.MessageActionRow()
-      .addComponent(accept_button)
-      .addComponent(deny_button);
+      localStorage.setItem(msg.author.id + ':status', 'PENDING')
+      localStorage.setItem(msg.author.id + ':request_id', request.id)
 
-    const embed = new Discord.MessageEmbed()
-      .setAuthor(msg.author.tag + ' requested verification:', msg.author.avatarURL())
-      .setColor(0xf2da80)
-      .setDescription(msg.content)
-      .addField('User Info:','<@' + msg.author.id + '>')
-      .setFooter('Pending')
-      .setTimestamp();
+    }
+    else if (localStorage.getItem(msg.author.id + ':status') === 'PENDING') {
+      const existing_request_id = localStorage.getItem(msg.author.id + ':request_id')
+      const existing_request = await mod_channel.messages.fetch(existing_request_id)
 
-    mod_channel.send('', {
-      component: row,
-      embed
-    });
+      const old_embed = existing_request.embeds[0]
+
+      const new_embed = old_embed
+        .setDescription(old_embed.description + '\n-----\n' + msg.content)
+        .setTimestamp()
+
+      existing_request.edit('', {
+        component: existing_request.components[0],
+        embed: new_embed
+      })
+    }
   }
 });
 
@@ -60,6 +89,9 @@ client.on('clickButton', async (button) => {
 
     const requester_tag = old_embed.fields[0].value
     const requester_id = requester_tag.slice(2,-1)
+
+    const requester = await guild.members.fetch(requester_id)
+    await requester.roles.add(member_role)
 
     const reply_embed = new Discord.MessageEmbed()
       .setColor(0x59e665)
@@ -77,6 +109,8 @@ client.on('clickButton', async (button) => {
 
     await button.message.edit(new_embed)
     await button.defer(true)
+
+    localStorage.setItem(requester_id + ':status', 'ACCEPTED')
   }
 });
 
@@ -104,6 +138,8 @@ client.on('clickButton', async (button) => {
 
     await button.message.edit(new_embed)
     await button.defer(true)
+
+    localStorage.setItem(requester_id + ':status', 'DENIED')
   }
 });
 
